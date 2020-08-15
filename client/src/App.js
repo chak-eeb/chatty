62c4450a143e53667dbcf503624c47cdfd9c658b
Chakib Atmane <chakib@Chakibs-MacBook-Pro.local>
2020-08-14 20:46:02 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+export class App extends Component {
+  render() {
+    return <div>Hello there</div>;
+  }
+}
+
+export default App;
